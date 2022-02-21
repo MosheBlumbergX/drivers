d93658368edc18f1b84e9f79262635937269ded7
mbl <mosheb@hotmail.co.uk>
2022-02-21 11:52:57 +0000

    update

---
+++ p2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+import re
+import sys
+def check_server(address, port):
+	# Create a TCP socket
+	s = socket.socket()
+	try:
+		s.connect((address, port))
+		return True
+	except socket.error as e:
+		print (e)
+		return False
+
+if __name__ == '__main__':
+	from optparse import OptionParser
+	parser = OptionParser()
+	
+	parser.add_option("-a", "--address", dest="address", default='localhost', help="ADDRESS for server", metavar="ADDRESS")
+	
+	parser.add_option("-p", "--port", dest="port", type="int", default=80, help="PORT for server", metavar="PORT")
+	
+	(options, args) = parser.parse_args()
+	print 'options: %s, args: %s' % (options, args)
+	check = check_server(options.address, options.port)
+	print 'check_server returned %s' % check
+	
+	sys.exit(not check)
