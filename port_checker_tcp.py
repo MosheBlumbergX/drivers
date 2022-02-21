8ed8b174a4b2028f8d8a9a38bd9c60a6404f0499
mbl <mosheb@hotmail.co.uk>
2022-02-21 11:57:00 +0000

    update

--- port_checker_tcp.py
+++ port_checker_tcp.py
@@ -7,11 +7,13 @@ import sys
 def check_server(address, port):
 	# Create a TCP socket
 	s = socket.socket()
+	print "Attempting to connect to %s on port %s" % (address, port)
 	try:
 		s.connect((address, port))
 		print "Connected to %s on port %s" % (address, port)
 		return True
-	except socket.error as e:
+	except socket.error, e:
+		print "Connection to %s on port %s failed: %s" % (address, port, e)
 		return False
 
 if __name__ == '__main__':
